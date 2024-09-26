4ec929c349889cecf755d337b5d1d9175fa0b45b
sj-dg <surajgjadhav1988+dg@gmail.com>
2024-09-26 11:14:14 +0530

    Prepare two json and shuffle random between the two when app is fetching them

--- src/utils/fetchRenderData.ts
+++ src/utils/fetchRenderData.ts
@@ -1,11 +1,17 @@
-import dataResponse from '@/data/response.json'
+import dataResponse1 from '@/data/response-1.json'
+import dataResponse2 from '@/data/response-2.json'
 import { type DataItem } from '@/types'
 
 type DataResponse = { data: DataItem[] }
+const dataCollection: DataResponse[] = [
+  dataResponse1 as DataResponse,
+  dataResponse2 as DataResponse
+]
 
 export default async (): Promise<DataResponse> => {
   return new Promise((resolve) => {
     setTimeout(() => {
+      const dataResponse = dataCollection[Math.round(Math.random())]
       /**
        * shuffle data everytime its invoked
        */
